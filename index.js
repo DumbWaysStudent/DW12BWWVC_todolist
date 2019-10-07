6fdc75b1658281e149c38281ba19c9539dc5a7e6
zaulsr <zaulsr14@gmail.com>
2019-10-07 16:51:18 +0700

    make todo force push

--- index.js
+++ index.js
@@ -4,9 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Todo from './src/Todo';
-import Todo2 from './src/Todo2';
-import MakeTodo from './src/MakeTodo';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => MakeTodo);
+AppRegistry.registerComponent(appName, () => App);
